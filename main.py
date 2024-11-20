030292eff3fb0ccfac95a255510e4716e02cc259
k15 <krisnantobiyuh@gmail.com>
2024-11-20 16:58:45 +0700

    feat : callback gitlab

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, HTTPException
+from fastapi.responses import JSONResponse
 from services.callback_gitlab import callback_gitlab
 from dotenv import load_dotenv
 
@@ -10,6 +11,19 @@ load_dotenv()
 
 @callback_route.post("/")
 async def root(request: dict):
-    return callback_gitlab(request)
+    try:
+        return callback_gitlab(request)
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=400, 
+            content={
+                "data": {},
+                "meta": {
+                    "code": "error",
+                    "message": "Error"
+                }
+            }
+        )
 
 app.include_router(callback_route)
