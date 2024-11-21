3904233b06048917d1926f742e518c2b539f7de5
k15 <krisnantobiyuh@gmail.com>
2024-11-21 17:43:30 +0700

    feat : helper get_telegram_chat

--- services/helper.py
+++ services/helper.py
@@ -3,3 +3,12 @@ import json
 def get_config_project(id: str):
     file = open(f"configs/projects/{id}.json")
     return json.load(file)
+
+def get_telegram_chat(project_id: str, username: str):
+    file = open(f".chatids/{project_id}/{username}.txt")
+    chat = file.read().split(":")
+
+    return {
+        "id": chat[0],
+        "username": chat[1],
+    }
