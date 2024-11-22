4b4945e2f0db5304a226d800938d802e6f83d236
k15 <krisnantobiyuh@gmail.com>
2024-11-22 17:31:16 +0700

    feat : config helper

---
+++ configs/config.py
@@ -0,0 +1,20 @@
+import json
+
+
+def get(project_id: str):
+    file = open(f"configs/projects/{project_id}.json")
+    return json.load(file)
+
+def get_gitlab_member_by_label(project_id: str, labels: str):
+    config = get(project_id)
+    for label in labels:
+        role = config.get("notify_to", {}).get(label, "")
+        members_gitlab_by_label = config.get("gitlab_role_username_members", {}).get(role, [])
+        return members_gitlab_by_label
+    
+    return []
+
+def get_labels(project_id: str):
+    config = get(project_id)
+    data = config.get("notify_to", {})
+    return [key for key, value in data.items()]
