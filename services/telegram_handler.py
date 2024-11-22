2068fcdd5dc7e178b2e6de3842c50c7008b98224
k15 <krisnantobiyuh@gmail.com>
2024-11-22 09:02:06 +0700

    fix : telegram /join command

--- services/telegram_handler.py
+++ services/telegram_handler.py
@@ -26,26 +26,34 @@ async def updater(data: dict):
     )
 
 async def join_bot(chat_id: int, username: str, message: str) -> None:
-    pattern = r'^/join \d+$'
+    pattern = r'^/join .+$'
     if re.match(pattern, message):
-        project_id = message.split(" ")[1]
+        pattern = r'^/join \d+:[a-zA-Z0-9]+$'
+        if re.match(pattern, message):
 
-        config = helper.get_config_project(project_id)
-        telegram_usernames = [item["telegram_username"] for item in config["members"]]
+            project_id = message.replace("/join ", "").split(":")[0]
+            gitlab_username = message.replace("/join ", "").split(":")[1]
+            
+            config = helper.get_config_project(project_id)
+            telegram_usernames = [item["telegram_username"] for item in config["members"]]
+            gitlab_usernames = [item["gitlab_username"] for item in config["members"]]
+            
+            if username in telegram_usernames and gitlab_username in gitlab_usernames:
+                directory = f"{CHAT_ID_PATH}/{project_id}"
+
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+
+                file = open(f"{directory}/{gitlab_username}.txt", "w")
+                file.write(f"{chat_id}:{username}:{gitlab_username}")
+
+                await bot.send_message(chat_id, f"You have joined project ID {project_id}")
+                return
+
+            await bot.send_message(chat_id, f"You are not member of project ID {project_id}")
         
-        if username in telegram_usernames:
-            directory = f"{CHAT_ID_PATH}/{project_id}"
-
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
-            file = open(f"{directory}/{username}.txt", "w")
-            file.write(f"{chat_id}:{username}")
-
-            await bot.send_message(chat_id, f"You have joined project ID {project_id}")
-            return
-
-        await bot.send_message(chat_id, f"You are not member of project ID {project_id}")
+        else:
+            await bot.send_message(chat_id, "Format join must be : /join gitlab_project_id:gitlab_username")
 
     else:
         await bot.send_message(chat_id, "Sorry, I don't know.")
