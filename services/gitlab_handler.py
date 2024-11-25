3751fd22d76b7a2e8d17a496e23c7d3e488f01d1
k15 <krisnantobiyuh@gmail.com>
2024-11-25 14:43:30 +0700

    fix : close handler

--- services/gitlab_handler.py
+++ services/gitlab_handler.py
@@ -20,6 +20,7 @@ async def updater(data: dict):
     
     if issue.type == "ISSUE":
         await issue_handler(
+            payload=data,
             project=project,
             issue=issue,
             changes=changes
@@ -31,16 +32,22 @@ async def updater(data: dict):
 # assignee member to issue
 async def issue_handler(**params):
 
+    payload = params.get("payload")
     project = params.get("project")
     issue   = params.get("issue")
     changes = params.get("changes")
+    action  = payload.get('object_attributes', {}).get('action')
+
     project_id = issue.project_id
+    title      = issue.title
+    issue_url  = issue.web_url
+    issue_id   = issue.iid
 
     notify_to = config.get_gitlab_usernames_by_label(project_id=project_id, labels=issue.labels)
-    
     if not issue.labels and issue.state == "opened":
         notify_to = config.get_gitlab_username_by_role(project_id=project_id, role="dev_team")
-        await notify_to_dev(notify_to, issue, changes)
+        msg = f"Selamat kamu dapat tugas baru, [Task #{issue_id}]({issue_url}). Mohon segera *dikerjakan* \n\n---\n {title}"
+        await notify_to_dev(notify_to, issue, message=msg)
 
     if "Dev Done" in issue.labels:
         await dev_done(project, notify_to, issue, changes)
@@ -57,13 +64,13 @@ async def issue_handler(**params):
         if author_username in tester_teams:
             await reopen(notify_to, issue)
 
+    if issue.state == "closed" and "close" in action:
+        notify_to = config.get_gitlab_username_by_role(project_id=project_id, role="dev_lead")
+        msg = f"[Task #{issue_id}]({issue_url}). Sudah selesai pengujian, mohon segera *dilakukan merge* \n\n---\n {title}"
+        await closed(notify_to=notify_to, issue=issue, project_id=project_id, message=msg)
 
 
-async def notify_to_dev(notify_to, issue, changes):
-    title      = issue.title
-    project_id = issue.project_id
-    issue_url  = issue.web_url
-    issue_id   = issue.iid
+async def notify_to_dev(notify_to, changes, message):
 
     if "assignees" in changes:
         username = changes.get("assignees", {}).get("current", []).pop().get("username", "")
@@ -71,9 +78,8 @@ async def notify_to_dev(notify_to, issue, changes):
             chat = helper.get_telegram_chat(project_id=project_id, gitlab_username=username)
             tele_user = chat.get("username")
             
-            text = f"Hi {tele_user}, Selamat kamu dapat tugas baru, [Task #{issue_id}]({issue_url}). Mohon segera *dikerjakan* \n\n---\n {title}"
+            text = f"Hi {tele_user}, {message}"
             await telegram_handler.send_text(chat.get("id"), text=text)
-
 
 
 async def dev_done(project, notify_to, issue, changes):
@@ -132,6 +138,7 @@ async def reopen(notify_to, issue):
             text = f"Hi {tele_user}, [Task #{issue_id}]({issue_url}) tidak lolos tes, di *Reopen* oleh @{tele_user_author}. Mohon segera *cek* dan *dikerjakan* \n\n---\n {title}"
             await telegram_handler.send_text(chat.get("id"), text=text)
 
+
 async def _notify_to_tester_team(assignees, project_id, message: str):
     for assignee in assignees:
         username = assignee.get("username", "")
@@ -143,3 +150,13 @@ async def _notify_to_tester_team(assignees, project_id, message: str):
 
             text = f"Hi {tele_user}, {message}"
             await telegram_handler.send_text(chat.get("id"), text=text)
+
+
+async def closed(notify_to: list, issue, project_id, message: str):
+    issue.labels = [label for label in issue.labels if label not in config.get_labels(project_id=project_id)]
+    issue.save()
+    for username in notify_to:
+        chat = helper.get_telegram_chat(project_id=project_id, gitlab_username=username)
+        tele_user = chat.get("username")
+        msg = f"Hi {tele_user}, {message}"
+        await telegram_handler.send_text(chat.get("id"), text=msg)
