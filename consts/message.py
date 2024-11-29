fd28471b4112e48fa76f1984ad8c864121762a55
k15 <krisnantobiyuh@gmail.com>
2024-11-29 13:41:45 +0700

    feat : const message

---
+++ consts/message.py
@@ -0,0 +1,21 @@
+WELCOME_MESSAGE = """
+👋 **Welcome to the Hanoman Devdsi!**
+
+This bot helps you manage your GitLab projects efficiently.
+"""
+
+HELP_MESSAGE = """
+🤖 *Hanoman Commands*
+
+❕ *Join GitLab Notifications*  
+  `/join <gitlab_project_id>:<gitlab_username>`  
+  _Subscribe to notifications for a specific GitLab project._
+"""
+
+# ✅ *View Task Details*  
+#   `/taskd <gitlab_project_id>:<gitlab_issue_id>`  
+#   _Get details about a specific task in a project._
+
+# ✅ *View My Tasks*
+#   `/mytask`  
+#   _See all tasks assigned to your GitLab username._
