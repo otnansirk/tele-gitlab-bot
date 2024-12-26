5c554027cfdd274168ef62670b88565242d8faff
k15 <krisnantobiyuh@gmail.com>
2024-12-26 13:37:34 +0700

    feat : webhook for daily meet notify

--- main.py
+++ main.py
@@ -2,82 +2,52 @@ from fastapi import FastAPI, APIRouter, Request, HTTPException
 from services import gitlab_handler
 from fastapi.responses import JSONResponse
 from services import telegram_handler
+from services import meet_hanlder
+from services import helper
 from dotenv import load_dotenv
 
 
 app = FastAPI()
-callback_route = APIRouter(prefix="/api/callbacks")
+callback_route = APIRouter(prefix="/api")
 
 load_dotenv()
-
-# @app.on_event("startup")
-# async def startup_event():
-#     """Set Telegram bot webhook."""
-#     await telegram_handler.set_webhook()
-
 
 @app.get("/")
 async def root():
     await telegram_handler.set_webhook()
     return "Service running"
 
-@callback_route.post("/gitlab")
+@callback_route.get("/dev-daily-meeting")
+async def dev_daily_meeting(request: Request):
+    try:
+        mentioned_member = request.query_params.get("mention")
+        await meet_hanlder.generate(meeting_name="mrp-dev-daily-meeting", title="MRP DEV Daily Meeting", message=mentioned_member)
+        return helper.res_success()
+    except Exception as e:
+        print(e, "ERROR /dev-daily-meeting")
+        return helper.res_error()
+
+@callback_route.post("/callbacks/gitlab")
 async def handle_webhook_gitlab(request: Request):
     try:
         data = await request.json()
         print("Request Gitlab : ", data)
         await gitlab_handler.updater(data)
-        return JSONResponse(
-            status_code=200, 
-            content={
-                "data": {},
-                "meta": {
-                    "code": "success",
-                    "message": "Success"
-                }
-            }
-        )
+        return helper.res_success(data)
     except Exception as e:
-        print(e)
-        return JSONResponse(
-            status_code=400, 
-            content={
-                "data": {},
-                "meta": {
-                    "code": "error",
-                    "message": "Error"
-                }
-            }
-        )
+        print(e, "ERROR /callbacks/gitlab")
+        return helper.res_error()
 
-@callback_route.post("/telegram")
+@callback_route.post("/callbacks/telegram")
 async def handle_webhook_telegram(request: Request):
     try:
         data = await request.json()
         print("Request Telegram : ", data)
         await telegram_handler.updater(data)
-        return JSONResponse(
-            status_code=200, 
-            content={
-                "data": data,
-                "meta": {
-                    "code": "success",
-                    "message": "Success"
-                }
-            }
-        )
+        return helper.res_success()
     except Exception as e:
-        print(e, "ERROR APA SIH")
-        return JSONResponse(
-            status_code=400, 
-            content={
-                "data": {},
-                "meta": {
-                    "code": "error",
-                    "message": "Error"
-                }
-            }
-        )
+        print(e, "ERROR /callbacks/telegram")
+        return helper.res_error()
 
 
 app.include_router(callback_route)
