6f2b30f8f4e460c228c86cd7def015b69c97f235
k15 <krisnantobiyuh@gmail.com>
2024-12-26 13:36:07 +0700

    fix: meeting handler generate url

--- services/meet_hanlder.py
+++ services/meet_hanlder.py
@@ -1,7 +1,7 @@
 from services import telegram_handler
 from core.db.database import Database
 
-async def generate(chat_id: str, title: str = "Meeting", message: str = "", meeting_name: str = "Urgent-Meeting"):
+async def generate(chat_id: str = "", title: str = "Meeting", message: str = "", meeting_name: str = "Urgent-Meeting"):
     base_url = "https://meet.jit.si/"
     meet_url = base_url + meeting_name
 
@@ -9,17 +9,17 @@ async def generate(chat_id: str, title: str = "Meeting", message: str = "", meet
 {title}
 Link : {meet_url}
 """
+    if chat_id:
+        await telegram_handler.send_text(chat_id=chat_id, text=text)
 
-    await telegram_handler.send_text(chat_id=chat_id, text=text)
-
-    usernames = message.split(" @")
+    usernames = message.split("@")
     db = Database()
     for username in usernames:
-        tele_account = db.fetch(table_name="telegram_account").select("*").eq("username", username).execute()
-        if len(tele_account.data):
-            chat_id = tele_account.data[0].get("chat_id")
-            await telegram_handler.send_text(chat_id=chat_id, text=text)
-
+        if username:
+            tele_account = db.fetch(table_name="telegram_account").select("*").eq("username", username.replace(" ", "")).execute()
+            if len(tele_account.data):
+                chat_id = tele_account.data[0].get("chat_id")
+                await telegram_handler.send_text(chat_id=chat_id, text=text)
     return "NO"
 
 async def my_teams(chat_id: str, username: str): 
